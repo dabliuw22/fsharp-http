a4fbbc4450e6b0b92d5f4bb64aadd9aa9f5585d0
Will Leyton <wleylop22@gmail.com>
2021-05-25 08:10:33 -0500

    Add Program

---
+++ src/App/Program.fs
@@ -0,0 +1,30 @@
+open Suave
+open Adapter.Logger
+open Adapter.Http.Products.Route
+open Application.Products
+open Adapter.Sql.Database.Config
+open Adapter.Sql.Database.Handler
+open Adapter.Sql.Products
+open Npgsql
+
+[<EntryPoint>]
+let main argv =
+
+    let _ = Log.info "Start Server"
+
+    use connection =
+        new NpgsqlConnection((DbConfig.fromEnv >> DbConfig.connectionStr) ())
+
+    connection.Open()
+
+    let db = new PgDatabaseHandler(connection)
+
+    let query = new Query.DefaultQueryProducts(db)
+
+    let command = new Command.DefaultCommandProducts(db)
+
+    let handle =
+        new Handle.DefaultProductsHandle(query, command)
+
+    startWebServer defaultConfig (app handle)
+    0
