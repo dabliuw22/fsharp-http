201883ee50cb04b895c9260aa1117729ed1217db
Will Leyton <wleylop22@gmail.com>
2021-05-27 09:09:21 -0500

    Delete

--- src/App/Domain/Result.fs
+++
@@ -1,6 +0,0 @@
-module Domain.Result
-
-let handle<'a, 'b> (failure: 'b) (bin: Choice<'a, exn>) : Result<'a, 'b> =
-    match bin with
-    | Choice1Of2 value -> Ok value
-    | Choice2Of2 _ -> Error failure
