39229afb67c6370cf0010ea95ae939d58cc1a682
Will Leyton <wleylop22@gmail.com>
2021-07-27 08:28:32 -0500

    Add Json policy

--- src/App/Adapter/Http/Json.fs
+++ src/App/Adapter/Http/Json.fs
@@ -2,14 +2,44 @@ namespace Adapter.Http.Json
 
 open System.Text.Json
 
+module Policy =
+    open System
+    open System.Linq
+    open System.Runtime.CompilerServices
+
+    [<Extension>]
+    type private JsonNamingExtension =
+        [<Extension>]
+        static member inline ToSnakeCase(name: string) =
+            String.Concat(
+                name.Select
+                    (fun x i ->
+                        if Char.IsUpper x then
+                            let lower = x |> Char.ToLower |> Char.ToString
+                            if i > 0 then "_" + lower else lower
+                        else
+                            Char.ToString x)
+            )
+
+    type private ToSnakeCase() =
+        inherit JsonNamingPolicy()
+        override _.ConvertName(name: string) = name.ToSnakeCase()
+
+
+    let toOptions =
+        JsonSerializerOptions(PropertyNamingPolicy = ToSnakeCase())
+
 module Serializer =
+
     let serialize<'a> (data: 'a) =
         try
+            // JsonSerializer.Serialize(data, toOptions) |> Some
             JsonSerializer.Serialize data |> Some
         with
         | _ -> None
 
 module Deserializer =
+
     let deserialize<'a> (json: string) =
         try
             JsonSerializer.Deserialize<'a> json |> Some
