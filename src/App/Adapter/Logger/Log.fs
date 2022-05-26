c258d75419ef1e634b0631e3cb2feb59c04d6dcf
Will Leyton <wleylop22@gmail.com>
2022-05-26 08:29:05 -0500

    Update Log

--- src/App/Adapter/Logger/Log.fs
+++ src/App/Adapter/Logger/Log.fs
@@ -1,39 +1,86 @@
 namespace Adapter.Logger
 
-open Microsoft.Extensions.Configuration
 open Serilog
-open Serilog.Configuration
-open Serilog.Events
 open Serilog.Formatting.Json
-open Serilog.Sinks
-open Serilog.Enrichers
 
+module Config =
+    type LogConfig = { Template: string; FilePath: string }
+
+    module FromEnv =
+        open Adapter.Env.Envs
+
+        [<Literal>]
+        let private logTemplate =
+            "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{MachineName}] [{Level:u3}] [{CorrelationId}] {Message:lj} {Properties}{NewLine}{Exception}"
+
+        [<Literal>]
+        let private localPath = "logs/log.json"
+
+        type LogConfigFromEnv() =
+            member this.From = (this :> FromEnv<LogConfig>).From
+
+            interface FromEnv<LogConfig> with
+                member _.From() =
+                    let template = getEnvWithDefault "LOG_TEMPLATE" id logTemplate
+
+                    let filePath = getEnvWithDefault "LOG_FILE_PATH" id localPath
+
+                    { Template = template
+                      FilePath = filePath }
+
+    module LogConfig =
+
+        open System.Runtime.CompilerServices
+
+        [<Extension; Sealed>]
+        type LogExtension =
+            [<Extension>]
+            static member inline ToLoggerConfiguration
+                (({ Template = template; FilePath = path }: LogConfig) as _)
+                : Core.Logger =
+                LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .Enrich.WithThreadId()
+                    .Enrich.WithThreadName()
+                    .Enrich.WithCorrelationId()
+                    .Enrich.WithMachineName()
+                    .WriteTo
+                    .Async(fun writer ->
+                        writer.Console(outputTemplate = template)
+                        |> ignore)
+                    .WriteTo.Async(fun writer -> writer.File(JsonFormatter(), path) |> ignore)
+                    .CreateLogger()
 
 module Log =
+    open Config.LogConfig
+    open System.Runtime.CompilerServices
 
-    let private template =
-        "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{MachineName}] [{Level:u3}] [{CorrelationId}] {Message:lj} {Properties}{NewLine}{Exception}"
+    let private logConfig = Config.FromEnv.LogConfigFromEnv().From()
 
-    let private logger =
-        LoggerConfiguration()
-            .MinimumLevel.Information()
-            .Enrich.WithThreadId()
-            .Enrich.WithThreadName()
-            .Enrich.WithCorrelationId()
-            .Enrich.WithMachineName()
-            .WriteTo.Console(outputTemplate = template)
-            .CreateLogger()
+    let logger () = logConfig.ToLoggerConfiguration()
 
-    type LogAction = string -> unit
+    type LogAction = string -> string -> unit
+
+    [<Extension; Sealed>]
+    type LoggerExtension =
+        [<Extension>]
+        static member inline GetLogger(log: Core.Logger) =
+            fun (ctx: string) -> log.ForContext("SourceContext", ctx)
+
+
 
     let info: LogAction =
-        fun msj -> using logger (fun log -> log.ForContext<Log>().Information msj)
+        fun ctx msj ->
+            using (logger ()) (fun log ->
+                log
+                    .ForContext("SourceContext", ctx)
+                    .Information msj)
 
     let error: LogAction =
-        fun msj -> using logger (fun log -> log.Error msj)
+        fun ctx msj -> using (logger ()) (fun log -> log.ForContext("SourceContext", ctx).Error msj)
 
     let warning: LogAction =
-        fun msj -> using logger (fun log -> log.Warning msj)
+        fun ctx msj -> using (logger ()) (fun log -> log.ForContext("SourceContext", ctx).Warning msj)
 
     let debug: LogAction =
-        fun msj -> using logger (fun log -> log.Debug msj)
+        fun ctx msj -> using (logger ()) (fun log -> log.ForContext("SourceContext", ctx).Debug msj)
