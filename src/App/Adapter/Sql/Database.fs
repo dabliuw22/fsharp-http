250d8ff26551101232605d0e6be65fb71976fd01
Will Leyton <wleylop22@gmail.com>
2022-05-27 08:36:01 -0500

    Update Database

--- src/App/Adapter/Sql/Database.fs
+++ src/App/Adapter/Sql/Database.fs
@@ -18,20 +18,15 @@ module Config =
 
             interface FromEnv<DbConfig> with
                 member _.From() =
-                    let host =
-                        getEnvWithDefault "DATABASE_HOST" id "localhost"
+                    let host = getEnvWithDefault "DATABASE_HOST" id "localhost"
 
-                    let username =
-                        getEnvWithDefault "DATABASE_USERNAME" id "fsharp"
+                    let username = getEnvWithDefault "DATABASE_USERNAME" id "fsharp"
 
-                    let password =
-                        getEnvWithDefault "DATABASE_PASSWORD" id "fsharp"
+                    let password = getEnvWithDefault "DATABASE_PASSWORD" id "fsharp"
 
-                    let database =
-                        getEnvWithDefault "DATABASE_NAME" id "fsharp_db"
+                    let database = getEnvWithDefault "DATABASE_NAME" id "fsharp_db"
 
-                    let port =
-                        getEnvWithDefault "DATABASE_PORT" int 5432
+                    let port = getEnvWithDefault "DATABASE_PORT" int 5432
 
                     { Host = host
                       Username = username
@@ -40,20 +35,29 @@ module Config =
                       Port = port }
 
     module DbConfig =
+        open Npgsql
+        open System.Runtime.CompilerServices
 
-        let toConnectionStr
-            { Host = host
-              Username = username
-              Password = pass
-              Database = name
-              Port = port }
-            =
-            Sql.host host
-            |> Sql.database name
-            |> Sql.username username
-            |> Sql.password pass
-            |> Sql.port port
-            |> Sql.formatConnectionString
+        [<Extension; Sealed>]
+        type DatabaseExtension =
+            [<Extension>]
+            static member inline ToConnectionStr
+                (({ Host = host
+                    Username = username
+                    Password = pass
+                    Database = name
+                    Port = port }: DbConfig) as _)
+                : string =
+                Sql.host host
+                |> Sql.database name
+                |> Sql.username username
+                |> Sql.password pass
+                |> Sql.port port
+                |> Sql.formatConnectionString
+
+            [<Extension>]
+            static member inline ToConnection(config: DbConfig) : NpgsqlConnection =
+                new NpgsqlConnection(config.ToConnectionStr())
 
 module Error =
     type DatabaseError =
@@ -65,51 +69,58 @@ module Error =
 
 module Handler =
     open Domain
+    open Domain.Result.Handler
 
-    type DatabaseHandler =
-        abstract member Query :
-            string -> ((string * SqlValue) list) -> (RowReader -> 'a) -> Async<Result<'a list, Error.DatabaseError>>
+    [<AbstractClass>]
+    type DatabaseHandler() =
+        abstract member Query:
+            string -> ((string * SqlValue) list) -> (RowReader -> 'a) -> AsyncResult<'a list, Error.DatabaseError>
 
-        abstract member Option :
-            string -> ((string * SqlValue) list) -> (RowReader -> 'a) -> Async<Result<'a option, Error.DatabaseError>>
+        abstract member Option:
+            string -> ((string * SqlValue) list) -> (RowReader -> 'a) -> AsyncResult<'a option, Error.DatabaseError>
 
-        abstract member Command : string -> ((string * SqlValue) list) -> Async<Result<int, Error.DatabaseError>>
+        abstract member Command: string -> ((string * SqlValue) list) -> AsyncResult<int, Error.DatabaseError>
 
-        abstract member CommandRow :
-            string -> ((string * SqlValue) list) -> (RowReader -> 'a) -> Async<Result<'a, Error.DatabaseError>>
+        abstract member CommandRow:
+            string -> ((string * SqlValue) list) -> (RowReader -> 'a) -> AsyncResult<'a, Error.DatabaseError>
 
+    [<Sealed>]
     type PgDatabaseHandler(conn: Npgsql.NpgsqlConnection) =
-        interface DatabaseHandler with
-            member _.Query query ``params`` mapper =
-                conn
-                |> Sql.existingConnection
-                |> Sql.query query
-                |> Sql.parameters ``params``
-                |> Sql.executeAsync mapper
-                |> Async.AwaitTask
-                |> Async.Catch
-                |> Async.map (Error.Handler.handle (Error.query $"Query Error: {query}"))
+        inherit DatabaseHandler()
 
-            member this.Option query ``params`` mapper =
-                (this :> DatabaseHandler).Query query ``params`` mapper
-                |> Async.map (Result.map List.tryHead)
+        override _.Query query ``params`` mapper =
+            conn
+            |> Sql.existingConnection
+            |> Sql.query query
+            |> Sql.prepare
+            |> Sql.parameters ``params``
+            |> Sql.executeAsync mapper
+            |> Async.AwaitTask
+            |> Async.Catch
+            |> AsyncResult.apply (Error.query $"Query Error: {query}")
 
-            member _.Command cmd ``params`` =
-                conn
-                |> Sql.existingConnection
-                |> Sql.query cmd
-                |> Sql.parameters ``params``
-                |> Sql.executeNonQueryAsync
-                |> Async.AwaitTask
-                |> Async.Catch
-                |> Async.map (Error.Handler.handle (Error.command $"Command Error: {cmd}"))
+        override this.Option query ``params`` mapper =
+            (this :> DatabaseHandler).Query query ``params`` mapper
+            |> AsyncResult.map (List.tryHead)
 
-            member _.CommandRow cmd ``params`` mapper =
-                conn
-                |> Sql.existingConnection
-                |> Sql.query cmd
-                |> Sql.parameters ``params``
-                |> Sql.executeRowAsync mapper
-                |> Async.AwaitTask
-                |> Async.Catch
-                |> Async.map (Error.Handler.handle (Error.command $"Command Error: {cmd}"))
+        override _.Command cmd ``params`` =
+            conn
+            |> Sql.existingConnection
+            |> Sql.query cmd
+            |> Sql.prepare
+            |> Sql.parameters ``params``
+            |> Sql.executeNonQueryAsync
+            |> Async.AwaitTask
+            |> Async.Catch
+            |> AsyncResult.apply (Error.command $"Command Error: {cmd}")
+
+        override _.CommandRow cmd ``params`` mapper =
+            conn
+            |> Sql.existingConnection
+            |> Sql.query cmd
+            |> Sql.prepare
+            |> Sql.parameters ``params``
+            |> Sql.executeRowAsync mapper
+            |> Async.AwaitTask
+            |> Async.Catch
+            |> AsyncResult.apply (Error.command $"Command Error: {cmd}")
