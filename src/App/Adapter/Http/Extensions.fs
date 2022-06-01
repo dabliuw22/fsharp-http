ba04f0342a8fb01ed2346163ac16ad04d4c53802
Will Leyton <wleylop22@gmail.com>
2022-06-01 08:56:59 -0500

    Update http extensions

--- src/App/Adapter/Http/Extensions.fs
+++ src/App/Adapter/Http/Extensions.fs
@@ -5,9 +5,10 @@ open Suave.Operators
 open Suave.Writers
 open System.Runtime.CompilerServices
 
+[<Literal>]
 let ApplicationJson = "application/json; charset=utf-8"
 
-[<Extension>]
+[<Extension; Sealed>]
 type WebPartExtension =
     [<Extension>]
     static member inline Json(webPart: HttpContext -> Async<HttpContext option>) =
